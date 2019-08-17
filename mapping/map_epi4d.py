f97cbff3bb3274400cd70973f618cf57e62a4e17
Daniel <you@example.com>
2019-08-17 22:22:28 +0200

    change time series mapping because of memory allocation problems

--- mapping/map_epi4d.py
+++ mapping/map_epi4d.py
@@ -2,19 +2,23 @@
 Transform time series to target space
 
 In the following script, epi time series in native space are transformed to a target space using a
-deformation field. The transformed time series get the prefix r.
+deformation field. The transformed time series get the prefix r. Because of heap size limits, the 
+time series is split and the déformation is applied separately to each volume.
 
 created by Daniel Haenelt
 Date created: 07-08-2019            
-Last modified: 07-08-2019  
+Last modified: 17-08-2019  
 """
 import os
+import shutil as sh
+import numpy as np
+import nibabel as nb
 from sh import gunzip
 from nighres.registration import apply_coordinate_mappings
 
 # input
 input_epi = [
-        "/home/raid2/haenelt/Desktop/udata_test.nii",
+        "/home/raid2/haenelt/Desktop/udata_test.nii.gz",
         ]
 
 input_reg = [
@@ -30,25 +34,56 @@ padding = "closest"
 # apply deformation    
 if len(input_epi) == len(input_reg):
     for i in range(len(input_epi)):
-        apply_coordinate_mappings(input_epi[i], 
-                                  input_reg[i], 
-                                  interpolation=interpolation, 
-                                  padding=padding,
-                                  save_data=True, 
-                                  overwrite=True, 
-                                  output_dir=os.path.dirname(input_epi[i]),
-                                  file_name=None,
-                                  )
+        
+        # make temporary output folder
+        tmp = np.random.randint(0, 10, 5)
+        tmp_string = ''.join(str(i) for i in tmp)
+        path_tmp = os.path.join(os.path.dirname(input_epi[i]),"tmp_"+tmp_string)
+        if not os.path.exists(path_tmp):
+            os.mkdir(path_tmp)
+        
+        # get length of time series
+        data = nb.load(input_epi[i])
+        data_array = data.get_fdata()
+        nt = nb.load(input_epi[i]).header["dim"][4]
+        data.header["dim"][4] = 1 # change header for single 3d volumes
+        
+        for j in range(nt):
+            
+            # save single time frame
+            output = nb.Nifti1Image(data_array[:,:,:,j], data.affine, data.header)
+            nb.save(output ,path_tmp,str(j)+".nii")
+            
+            apply_coordinate_mappings(os.path.join(path_tmp,str(j)+".nii"), 
+                                      input_reg[i], 
+                                      interpolation=interpolation, 
+                                      padding=padding,
+                                      save_data=True, 
+                                      overwrite=True, 
+                                      output_dir=path_tmp,
+                                      file_name=None,
+                                      )
+    
+            # unzip output
+            gunzip(os.path.join(path_tmp,str(j)+"_def-img.nii.gz"))
+
+        # merge final deformed time series
+        data = nb.load(path_tmp,"0_def-img.nii")
+        data.header["dim"][4] = nt
+        data_res = np.zeros(data.header["dim"][1:5])
+        
+        for j in range(nt):
+            data_res[:,:,:,j] = nb.load(os.path.join(path_tmp,str(j)+"_deg-img.nii")).get_fdata()            
 
         # time series path and basename
         path = os.path.dirname(input_epi[i])
         file = os.path.splitext(os.path.basename(input_epi[i]))[0]
-
-        # unzip output
-        gunzip(os.path.join(path,file+"_def-img.nii.gz"))
         
-        # rename output time series
-        os.rename(os.path.join(path,file+"_def-img.nii"),os.path.join(path,"r"+file+".nii"))
+        output = nb.Nifti1Image(data_res, data.affine, data.header)
+        nb.save(output, os.path.join(path, "r"+file+".nii"))
+
+        # delete intermediate files
+        sh.rmtree(path_tmp, ignore_errors=True)
 
 else:
     print("Number of time series and deformation are not the same!")
