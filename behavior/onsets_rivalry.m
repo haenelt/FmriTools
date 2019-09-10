e9d1789c8843bae306d5c0c8e0be2c23b96debe2
Daniel <you@example.com>
2019-09-10 17:05:05 +0200

    update

--- behavior/onsets_rivalry.m
+++ behavior/onsets_rivalry.m
@@ -3,24 +3,25 @@
 % From the condition logfile of the binocular rivalry paradigm, onsets and
 % durations for the following GLM analysis are computed. Onsets are split
 % into rest, left eye, right eye or piecemeal periods. Onsets are excluded
-% if their durations are below a set time threshold. Optionally, an mixed
-% (n_regressor = 4) and and outlier (n_regressors = 5) regressors are
+% if their durations are below a set time threshold and if they are not
+% neighboured by correct switches (i.e., if they are neighboured by
+% the same button presses). Optionally, a mixed (save_mixed) regressor is
 % written out.
 %
-% In the experiment, participant wear the red filter over their left eye
+% In the experiment, participants wear the red filter over their left eye
 % and the green filter over their right eye. In SwitchData, button 
 % responses of 2 and 1 refer to red and green onsets, respectively.
 %
 % created by Daniel Haenelt
 % Date created: 05-08-2019         
-% Last modified: 05-08-2019  
+% Last modified: 10-09-2019  
 
 % input of condition files
-input = {'/home/daniel/Schreibtisch/p2_GE_EPI1_Run3_rivalry_Cond.mat'};
+input = {'/home/daniel/Schreibtisch/p3_GE_EPI1_Run5_rivalry_Cond.mat'};
 
 % parameters
 response_threshold = 2; % in s
-n_regressors = 3; % number of regressors (3-5)
+save_mixed = false;
 
 %%% do not edit below %%%
 
@@ -33,41 +34,31 @@ for i = 1:length(input)
     % load mat file
     data = load(input{i});
     
-    % clean switches (delete repeating entries)
-    data_repeat = zeros(length(data.SwitchData(:,1)),1);
-    data_repeat(1) = 1;
-    for j = 2:length(data.SwitchData(:,1))
-        if data.SwitchData(j-1,1) == data.SwitchData(j,1)
-            data_repeat(j) = 0;
+    % clean switches (only consider time points with correct neighbouring)
+    data_consider = zeros(length(data.SwitchData(:,1)),1);
+    if data.SwitchData(1,1) ~= data.SwitchData(2,1) 
+        data_consider(1) = 1;
+    end
+    for j = 2:length(data.SwitchData(:,1)) - 1
+        if data.SwitchData(j-1,1) ~= data.SwitchData(j,1) && data.SwitchData(j,1) ~= data.SwitchData(j+1,1)
+            data_consider(j) = 1;
         else
-            data_repeat(j) = 1;
+            data_consider(j) = 0;
         end
     end
     
     % sum responses
-    response = [4 ; data.SwitchData(data_repeat ~= 0,1)]; % 4 until first switch
-    time1 = [data.onsets{2}(1) ; data.SwitchData(data_repeat ~= 0,2)];
-    time2 = [time1(2:end) ; data.onsets{1}(2)];
+    response = data.SwitchData(data_consider ~= 0,1);
+    time1 = data.SwitchData(data_consider ~= 0,2);
+    time2 = data.SwitchData(find(data_consider ~= 0) + 1,2);
     time_delta = time2 - time1;
     
-    % threshold responses
+    % threshold responses (outliers get the response 4 but are not written
+    % out)
     response(time_delta < response_threshold) = 4;
     
     % save output as mat file
-    if n_regressors == 3
-        names = cell(1,3);
-        names{1} = 'rest';
-        names{2} = 'left';
-        names{3} = 'right';
-        onsets = cell(1,3);
-        onsets{1} = data.onsets{1}';
-        onsets{2} = time1(response==2);
-        onsets{3} = time1(response==1);
-        durations = cell(1,3);
-        durations{1} = [data.durations{1} ; data.durations{1}];
-        durations{2} = time_delta(response==2);
-        durations{3} = time_delta(response==1);
-    elseif n_regressor == 4
+    if save_mixed
         names = cell(1,4);
         names{1} = 'rest';
         names{2} = 'left';
@@ -83,27 +74,21 @@ for i = 1:length(input)
         durations{2} = time_delta(response==2);
         durations{3} = time_delta(response==1);
         durations{4} = time_delta(response==3);
-    elseif n_regressor == 5
-        names = cell(1,5);
+    else
+        names = cell(1,3);
         names{1} = 'rest';
         names{2} = 'left';
         names{3} = 'right';
-        names{4} = 'mixed';
-        names{5} = 'outlier';
-        onsets = cell(1,5);
+        onsets = cell(1,3);
         onsets{1} = data.onsets{1}';
         onsets{2} = time1(response==2);
         onsets{3} = time1(response==1);
-        onsets{4} = time1(response==3);
-        onsets{5} = time1(response==4);
-        durations = cell(1,5);
+        durations = cell(1,3);
         durations{1} = [data.durations{1} ; data.durations{1}];
         durations{2} = time_delta(response==2);
         durations{3} = time_delta(response==1);
-        durations{4} = time_delta(response==3);
-        durations{5} = time_delta(response==4);
     end
-
+    
     % save finale condition file
     save(fullfile(pathfile,[name '_threshold' ext]),'names','onsets','durations');
     
@@ -113,7 +98,6 @@ for i = 1:length(input)
     left    = [ceil(nscale*time1(response==2)) ceil(nscale*time1(response==2) + nscale*time_delta(response==2))];
     right   = [ceil(nscale*time1(response==1)) ceil(nscale*time1(response==1) + nscale*time_delta(response==1))];
     mixed   = [ceil(nscale*time1(response==3)) ceil(nscale*time1(response==3) + nscale*time_delta(response==3))];
-    outlier = [ceil(nscale*time1(response==4)) ceil(nscale*time1(response==4) + nscale*time_delta(response==4))];
     
     M = zeros(1,1,3);
     for j = 1:length(rest(:,1))
@@ -128,18 +112,17 @@ for i = 1:length(input)
     for j = 1:length(mixed(:,1))
         M(mixed(j,1):mixed(j,2),4,:) = 1;
     end
-    for j = 1:length(outlier(:,1))
-        M(outlier(j,1):outlier(j,2),5,:) = 1;
-    end
     
     % plot design matrix
     figure(i)
+    set(gca,'visible','off');
     imagesc(M);
-    names = {'baseline'; 'left'; 'right'; 'mixed'; 'outlier'};
-    set(gca,'xtick',1:5,'xticklabel',names);
+    names = {'baseline'; 'left'; 'right'; 'mixed'};
+    set(gca,'xtick',1:4,'xticklabel',names);
     yt = arrayfun(@num2str,get(gca,'ytick')/nscale,'un',0);
     set(gca,'yticklabel',yt);
     ylabel('time in TR');
     title(['Design matrix for run ' num2str(i)]);
+    saveas(gca,fullfile(pathfile,[name '_plot.png']));
     
 end
