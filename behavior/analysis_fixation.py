abeafa9d896c32a0ead4368d4c05d244f024ad27
Daniel <you@example.com>
2019-10-01 10:41:37 +0200

    add behavioural data analysis

---
+++ behavior/analysis_fixation.py
@@ -0,0 +1,85 @@
+"""
+Analysis of behavioural data
+
+Analysis of the behavioural performance of the dummy fixation task during scanning. The script 
+essentially performs the same calculation as directly done automatically after single functional 
+runs.
+
+created by Daniel Haenelt
+Date created: 01-10-2019             
+Last modified: 01-10-2019  
+"""
+import os
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+input = [
+        "/home/daniel/Schreibtisch/p1_GE_EPI1_Run1_odc.mat",
+         ]
+
+""" do not edit below """
+
+# font parameters for plots
+rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+
+for i in range(len(input)):
+    
+    # get output path and filename
+    path_output = os.path.dirname(input[i])
+    
+    # get mat-file
+    FixationData = loadmat(input[i])["FixationData"]
+
+    # number of responses
+    response = 0
+    change = 0
+    for j in range(len(FixationData)):
+        if FixationData[j,0] == 3:
+            response += 1
+        else:
+            change += 1
+
+    # number of hits, misses and rt
+    change_miss = 0
+    change_hit = 0
+    rt = []
+    for j in range(len(FixationData)-1):
+        if FixationData[j,0] != 3 and FixationData[j+1,0] != 3:
+            change_miss += 1
+        elif FixationData[j,0] != 3 and FixationData[j+1,0] == 3:
+            change_hit += 1
+            rt.append((FixationData[j+1,1]- FixationData[j,1])*1000)
+
+    # add a miss if the run does not end with a response
+    if FixationData[-1,0] != 3:
+        change_miss += 1
+
+    # compute error rate
+    error_rate = change_miss / change * 100
+
+    # reaction time mean and std
+    mean_rt = np.mean(rt)
+    std_rt = np.std(rt)
+
+    # output
+    fileID = open(os.path.join(path_output,"fixation_task_summary.txt"),"w")
+    fileID.write("Number of changes: %i\n" % change)
+    fileID.write("Number of responses: %i\n" % response)
+    fileID.write("Number of hits: %i\n" % change_hit)
+    fileID.write("Number of misses: %i\n" % change_miss)
+    fileID.write("Error rate: %.2f %%\n" % error_rate)
+    fileID.write("Mean RT: %.2f ms\n" % mean_rt)
+    fileID.write("Corresponding SD: %.2f ms" % std_rt)
+    fileID.close()
+
+    # hist plot
+    fig, ax = plt.subplots()
+    ax.hist(rt)
+    ax.set_xlabel("RT in ms")
+    ax.set_ylabel("Number of responses")
+    ax.set_title("Dummy fixation task reaction times")
+    fig.savefig(os.path.join(path_output,"fixation_task_hist.png"), format='png', bbox_inches='tight')
+    #plt.show()
