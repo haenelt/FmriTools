fcd276bdd25425e2ab24bfb5695a48f051662bd2
Daniel Haenelt <haenelt@marillion.cbs.mpg.de>
2019-10-26 20:36:19 +0200

    fix bugs

--- mapping/ana2epi_surf.py
+++ mapping/ana2epi_surf.py
@@ -15,32 +15,14 @@ import os
 from lib.mapping.deform_surface import deform_surface
 
 # input files
-input_surf = ["/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer0",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer1",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer2",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer3",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer4",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer5",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer6",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer7",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer8",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/lh.layer9",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer0",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer1",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer2",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer3",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer4",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer5",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer6",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer7",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer8",
-              "/data/pt_01880/V2STRIPES/p7/anatomy/layer/rh.layer9",
+input_surf = ["/data/pt_01880/Experiment1_ODC/p3/anatomy/layer/lh.layer5",
+              "/data/pt_01880/Experiment1_ODC/p3/anatomy/layer/rh.layer5",
               ]
 
-input_orig = "/data/pt_01880/V2STRIPES/p7/anatomy/freesurfer/mri/orig.mgz"
-input_deform = "/data/pt_01880/V2STRIPES/p7/deformation/colour/ge_epi1/epi2orig.nii.gz"
-input_target = "/data/pt_01880/V2STRIPES/p7/colour/GE_EPI1/Run_1/meanudata.nii"
-path_output = "/data/pt_01880/V2STRIPES/p7/anatomy/layer_def/colour/ge_epi1"
+input_orig = "/data/pt_01880/Experiment1_ODC/p3/anatomy/freesurfer/mri/orig.mgz"
+input_deform = "/data/pt_01880/Experiment1_ODC/p3/deformation/odc/GE_EPI1/epi2orig.nii.gz"
+input_target = "/data/pt_01880/Experiment1_ODC/p3/odc/GE_EPI1/diagnosis/mean_data.nii"
+path_output = "/data/pt_01880/test/non_rigid"
 
 """ do not edit below """
 
