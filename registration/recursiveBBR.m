a99e26b8700510f5b2daf5f48e5f95269f65b53d
Daniel <you@example.com>
2019-12-10 12:07:27 +0100

    add recursive bbr

---
+++ registration/recursiveBBR.m
@@ -0,0 +1,96 @@
+% recursive boundary-based registration
+
+% This script performs recurive BBR to register a functional data set with
+% surfaces generated from a separate whole-brain anatomy.
+
+% parameter mode (explanation from one of Tim vanMourik's scripts:
+% `r' for rotation around all axes, `rx', `ry', or `rz' for a single rotation around the respective 
+% axis and for example `rxrz' for a rotation around the x-axis and the z-axis. In the same way the 
+% scaling and translation can be modified. For example the combination `rystytz' would optimise for 
+% a rotation around the y-axis, a scaling in all direction and a translation along the y-axis and 
+% along the z-axis. The default is 'rst' for all transformations.
+
+% created by Daniel Haenelt
+% Date created: 09-12-2019
+% Last modified: 09-12-2019
+
+% input surfaces
+input_white = {
+    '/home/daniel/Schreibtisch/test/lh.layer10_def',...
+    '/home/daniel/Schreibtisch/test/rh.layer10_def',...
+    };
+
+input_pial = {
+    '/home/daniel/Schreibtisch/test/lh.layer0_def',...
+    '/home/daniel/Schreibtisch/test/rh.layer0_def',...
+    };
+
+% parameters
+subjects_dir = '/home/daniel/Schreibtisch';
+ref_vol = 'test/mean_data.nii';
+mask = '';
+min_vox = 10;
+min_vtx = 500;
+cuboid = true;
+tetrahedra = false;
+nn_smooth = 0.5;
+registration_mode = 'rst';
+reverse_contrast = true; % if inner surface is darker than outer surface
+cost_method = 'GreveFischl'; % GreveFischl, sum, sumOfSquares
+contrast_method = 'gradient'; % gradient, average, fixedDistance, relativeDistance, extrema
+
+% output filenames
+output_surf = 'boundaries_out.mat';
+output_cmap = 'sd.nii';
+
+% path
+pathSPM = '/home/daniel/source/spm12';
+pathFSURF = '/usr/local/freesurfer';
+pathMOURIK = '/home/daniel/projects/OpenFmriAnalysis';
+pathLIB = '/home/daniel/projects/scripts/lib';
+
+%%% do not edit below %%%
+
+% file separator
+fs = filesep;
+
+% add paths to the interpreter's search path
+addpath(genpath(pathSPM));
+addpath(genpath(pathFSURF));
+addpath(genpath(pathMOURIK));
+addpath(genpath(pathLIB));
+
+% parameters for bbr registration
+cfg = [];
+cfg.i_SubjectDirectory = subjects_dir;
+cfg.i_ReferenceVolume = ref_vol;
+cfg.i_Boundaries = 'boundaries_in.mat';
+cfg.i_Mask = mask;
+cfg.i_MinimumVoxels = min_vox;
+cfg.i_MinimumVertices = min_vtx;
+cfg.i_CuboidElements = cuboid;
+cfg.i_Tetrahedra = tetrahedra;
+cfg.i_NeighbourSmoothing = nn_smooth;
+cfg.i_Mode = registration_mode;
+cfg.i_ReverseContrast = reverse_contrast;
+cfg.i_OptimisationMethod = cost_method;
+cfg.i_ContrastMethod = contrast_method;
+cfg.o_Boundaries = output_surf;
+cfg.o_DisplacementMap = output_cmap;
+
+% surf2mat
+save_output = [cfg.i_SubjectDirectory fs cfg.i_Boundaries];
+[wSurface, pSurface, faceData] = surf2mat(input_white, input_pial, save_output);
+
+% registration
+tvm_recursiveBoundaryRegistration(cfg);
+
+% load boundary output
+load([cfg.i_SubjectDirectory fs cfg.o_Boundaries]);
+
+% write surfaces
+hemi = {'lh','rh'};
+for i = 1:length(hemi)
+    write_surf([cfg.i_SubjectDirectory fs hemi{i} '.white_def'], wSurface{i}(:,1:3), faceData{i});
+    write_surf([cfg.i_SubjectDirectory fs hemi{i} '.pial_def'], pSurface{i}(:,1:3), faceData{i});
+end
--- surface/match_vertex.py
+++ surface/match_vertex.py
@@ -1,60 +1,39 @@
 """
-Apply nonlinear deformation of surface to target space
+Match vertex numbers
 
-The purpose of the following script is to deform a list or surface meshs to a target space using
-a coordinate mapping.
-
-Before running the script, login to queen via ssh and set the freesurfer environment by calling 
-FREESURFER in the terminal.
+The purpose of the following script is to match the number of vertices between deformed white and
+pial surfaces. Vertices are removed which are not found in both surfaces. After removing, faces 
+vertex-in-orig indices are updated.
 
 created by Daniel Haenelt
-Date created: 07-02-2019
-Last modified: 09-12-2019
+Date created: 10-12-2019
+Last modified: 10-12-2019
 """
-from os.path import join, basename, splitext
-from lib.io import get_filename
-from lib.cmap import remove_edge_cmap
-from lib.surface import deform_surface, remove_vertex_outliers
+from lib.surface import match_vertex_number
 
 # input files
-input_surf = ["/home/daniel/Schreibtisch/temo/lh.layer0",
-              ]
+input_white_surf = ["/home/daniel/Schreibtisch/temp/lh.layer10_def",
+                    "/home/daniel/Schreibtisch/temp/rh.layer10_def",
+                    ]
 
-input_orig = "/home/daniel/Schreibtisch/temo/orig.mgz"
-input_deform = "/home/daniel/Schreibtisch/temo/epi2orig_edge.nii.gz" # epi2orig
-input_target = "/home/daniel/Schreibtisch/temo/mean_data.nii"
-path_output = "/home/daniel/Schreibtisch/output"
+input_pial_surf = ["/home/daniel/Schreibtisch/temp/lh.layer0_def",
+                   "/home/daniel/Schreibtisch/temp/rh.layer0_def",
+                   ]
 
-remove_edge = False
+input_white_ind = ["/home/daniel/Schreibtisch/temp/lh.layer10_ind.txt",
+                   "/home/daniel/Schreibtisch/temp/rh.layer10_ind.txt",
+                   ]
+
+input_pial_ind = ["/home/daniel/Schreibtisch/temp/lh.layer0_ind.txt",
+                  "/home/daniel/Schreibtisch/temp/rh.layer0_ind.txt",
+                  ]
 
 """ do not edit below """
 
-for i in range(len(input_surf)):
+for i in range(len(input_white_surf)):
     
-    # get hemisphere from input surface file name
-    hemi = splitext(basename(input_surf[i]))[0]
-    
-    # get path, basename and file extension from coordionate map
-    path_deform, name_deform, ext_deform = get_filename(input_deform)
-    
-    # remove edges from cmap
-    if remove_edge:
-        remove_edge_cmap(input_deform, 5, 5)
-        
-        # rename coordinate map basename
-        name_deform += "_edge"
-    
-    # deform surface
-    deform_surface(input_surf[i], 
-                   input_orig, 
-                   join(path_deform, name_deform+ext_deform), 
-                   input_target, 
-                   hemi, 
-                   path_output, 
-                   True)
-    
-    # remove vertex outliers
-    remove_vertex_outliers(join(path_output,basename(input_surf[i])+"_def"), 
-                           join(path_output,basename(input_surf[i])+"_ind.txt"), 
-                           5, 
-                           True)
+    # match vertex numbers
+    match_vertex_number(input_white_surf, 
+                        input_pial_surf, 
+                        input_white_ind, 
+                        input_pial_ind)
