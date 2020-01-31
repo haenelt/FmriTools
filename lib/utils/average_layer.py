82844e2f0f0b04fc9f71a46bc7384e78f6eac90b
Daniel Haenelt <haenelt@actinium.cbs.mpg.de>
2020-01-31 21:35:12 +0100

    update

--- lib/utils/average_layer.py
+++ lib/utils/average_layer.py
@@ -1,31 +1,43 @@
-def average_layer(img_input, path_output, basename_output):
+def average_layer(img_input, path_output, basename_output, mode="mean"):
     """
-    This averages data across different layers. Input arrays should be in mgh format. The output 
-    gets the suffix _avg.
+    This averages data across different layers or sessions. Input arrays should be in mgh format. 
+    The output gets the suffix of the chosen mode.
     Inputs:
         *img_input: list of input layers.
         *path_output: path where output is saved.
         *basename_output: basename of written output file.
+        *mode: average mode (mean or median).
     
     created by Daniel Haenelt
     Date created: 25-10-2019
-    Last modified: 25-10-2019
+    Last modified: 31-01-2020
     """
+    import sys
     import os
     import numpy as np
     import nibabel as nb
     
     # initialise array
     data = nb.load(img_input[0])
-    data_array = np.zeros_like(data.get_fdata())
+    data_size = data.header["dims"][0]
+    data_res = np.zeros((data_size,len(img_input)))
     
-    # sum over layers
+    # collect input arrays
     for i in range(len(img_input)):
-        data_array += nb.load(img_input[i]).get_fdata()    
-
-    # divide by number of layers
-    data_array = data_array / len(img_input)
+        data_res[:,i] = nb.load(img_input[i]).get_fdata()[:,0,0]
+    
+    # average
+    if mode is "mean":
+        data_res = np.mean(data_res, axis=1)
+    elif mode is "median":
+        data_res = np.median(data_res, axis=1)
+    else:
+        sys.exit("Choose a valid mode!")
+    
+    # expand dimensions to match with input array
+    data_res = np.expand_dims(data_res, axis=1)
+    data_res = np.expand_dims(data_res, axis=1)
     
     # write output file
-    output = nb.Nifti1Image(data_array,data.affine,data.header)
-    nb.save(output,os.path.join(path_output,basename_output+"_avg.mgh"))
+    output = nb.Nifti1Image(data_res, data.affine, data.header)
+    nb.save(output,os.path.join(path_output, basename_output+"_"+mode+".mgh"))
--- mapping/map2ana.py
+++ mapping/map2ana.py
@@ -2,67 +2,114 @@
 Map2ana
 
 In the following script, source data is transformed using a deformation field and the transformed
-data is mapped onto a surface mesh in the target space.
+data is mapped onto a surface mesh in the target space. The deformation of the input data is 
+optional.
 
 Before running the script, login to queen via ssh and set the freesurfer environments by calling 
 FREESURFER in the terminal.
 
 created by Daniel Haenelt
 Date created: 06-03-2019            
-Last modified: 02-09-2019  
+Last modified: 31-01-2020  
 """
 import os
 from nighres.registration import apply_coordinate_mappings
+from lib.io.get_filename import get_filename
 from lib.mapping import map2surface
 
 # input
-input_file = [        
-        "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/retinotopy/avg/native/ecc_imag_avg.nii",
-        "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/retinotopy/avg/native/ecc_phase_avg.nii",
-        "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/retinotopy/avg/native/ecc_real_avg.nii",
-        "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/retinotopy/avg/native/pol_imag_avg.nii",
-        "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/retinotopy/avg/native/pol_phase_avg.nii",
-        "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/retinotopy/avg/native/pol_real_avg.nii",
+file_in = [        
+        "/data/pt_01880/Experiment3_Stripes/p3/mpm/pd_kp_mtflash3d_v1ax_0p5_0008/Results/s2803151-124913-00001-00352-1_PD_gnlcorr_scaled_inverted.nii",
+        "/data/pt_01880/Experiment3_Stripes/p3/mpm/pd_kp_mtflash3d_v1ax_0p5_0008/Results/s2803151-124913-00001-00352-1_R1_gnlcorr.nii",
+        "/data/pt_01880/Experiment3_Stripes/p3/mpm/pd_kp_mtflash3d_v1ax_0p5_0008/Results/s2803151-124913-00001-00352-1_R2s_WOLS_gnlcorr_scaled.nii",
         ]
 
-input_surf = [
-        "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/anatomy/freesurfer/surf/lh.0p50",
-        "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/anatomy/freesurfer/surf/rh.0p50",
+surf_in = [
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer0_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer1_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer2_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer3_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer4_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer5_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer6_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer7_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer8_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer9_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer10_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer11_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer12_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer13_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer14_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer15_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer16_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer17_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer18_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer19_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/lh.layer20_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer0_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer1_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer2_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer3_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer4_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer5_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer6_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer7_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer8_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer9_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer10_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer11_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer12_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer13_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer14_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer15_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer16_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer17_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer18_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer19_mpm",
+        "/data/pt_01880/Experiment3_Stripes/p3/anatomy/layer_mpm/rh.layer20_mpm",
         ]
 
 # parameters
-deformation = "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/deformation/retinotopy/epi2orig.nii.gz"
-path_output = "/nobackup/actinium2/haenelt/ForOthers/RetinotopyFakhereh4/26958.af/retinotopy/avg"
+deformation_in = None
+path_output = "/data/pt_01880/Experiment3_Stripes/p3/mpm/pd_kp_mtflash3d_v1ax_0p5_0008/Results"
 interpolation = "linear" # can be linear or nearest
 
 """ do not edit below """
 
-# apply deformation    
+# output folders
 path_def = os.path.join(path_output,"def")
-for i in range(len(input_file)):
-    apply_coordinate_mappings(input_file[i], 
-                              deformation, 
-                              interpolation=interpolation, 
-                              padding='closest', 
-                              save_data=True, 
-                              overwrite=True, 
-                              output_dir=path_def,
-                              file_name=None,
-                              )
-
-# map to ana
 path_surf = os.path.join(path_output,"surf")
-for i in range(len(input_file)):
-    # name of input volume
-    filename_def = os.path.splitext((os.path.basename(input_file[i])))[0]
 
-    for j in range(len(input_surf)):
+for i in range(len(file_in)):
+    
+    # apply deformation    
+    if deformation_in is not None:
+        apply_coordinate_mappings(file_in[i], 
+                                  deformation_in, 
+                                  interpolation=interpolation, 
+                                  padding='closest', 
+                                  save_data=True, 
+                                  overwrite=True, 
+                                  output_dir=path_def,
+                                  file_name=None,
+                                  )
+    
+    # get filename of deformed file
+    if deformation_in is not None:
+        _, name_file, _ = get_filename(file_in[i])
+        filename_def = os.path.join(path_def, name_file+"_def-img.nii.gz") 
+    else:
+        filename_def = file_in[i]
+        
+    # map to ana
+    for j in range(len(surf_in)):
+        
         # hemisphere
-        hemi = os.path.splitext(os.path.basename(input_surf[j]))[0]
+        hemi = os.path.splitext(os.path.basename(surf_in[j]))[0]
     
         # sample on surface
-        map2surface(input_surf[j], 
-                    os.path.join(path_def,filename_def+"_def-img.nii.gz"), 
+        map2surface(surf_in[j], 
+                    filename_def, 
                     hemi, 
                     path_surf, 
                     input_white=None, 
