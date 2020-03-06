655c0c37535ff5901ab6f3b7e6566eb41c2635b7
Daniel <you@example.com>
2020-03-06 10:29:01 +0100

    add relief plot

---
+++ misc/make_relief.py
@@ -0,0 +1,44 @@
+"""
+Relief plot on surface mesh
+
+In the following script, a contrast is plotted as a relief on a corresponding surface mesh. The
+contrast data is filtered by a sigmoid function.
+
+created by Daniel Haenelt
+Date created: 06-03.2020         
+Last modified: 06-03-2020  
+"""
+import numpy as np
+import nibabel as nb
+from nibabel.freesurfer.io import read_geometry
+from nibabel.freesurfer.io import write_geometry
+from lib_gbb.normal.get_normal import get_normal
+
+file_surf = "/home/daniel/projects/column_analysis/test_data/lh.refined_enhanced_inflated"
+file_data = "/home/daniel/projects/column_analysis/test_data/lh.spmT_left_right_GE_EPI2_upsampled_avg_layer4_16.mgh"
+file_label = "/home/daniel/projects/column_analysis/test_data/lh.v1.label"
+file_out = "/home/daniel/Schreibtisch/relief"
+scale_factor = 2
+
+""" do not edit below """
+
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
+
+# load geometry
+vtx, fac = read_geometry(file_surf)
+
+# load contrast
+data = np.zeros_like(vtx)
+data[:,0] = nb.load(file_data).get_fdata()[:,0,0]
+data[:,1] = nb.load(file_data).get_fdata()[:,0,0]
+data[:,2] = nb.load(file_data).get_fdata()[:,0,0]
+
+# get normals
+normal = get_normal(vtx, fac)
+
+# make relief
+vtx += scale_factor * sigmoid(data) * normal
+
+# write output
+write_geometry(file_out, vtx, fac)
