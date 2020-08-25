9b1b2dd91b36fed786f1ad91e599400fac54548a
Daniel Haenelt <haenelt@actinium.cbs.mpg.de>
2020-08-25 10:49:40 +0200

    remove nipype from surface smoothing

--- lib/surface/smooth_surface.py
+++ lib/surface/smooth_surface.py
@@ -1,6 +1,6 @@
 def smooth_surface(file_in, file_out, n_iter):
     """
-    This function smoothes a surface file using freesurfer.
+    This function smoothes a surface mesh using freesurfer.
     Inputs:
         *file_in: filename of input surface.
         *file_out: filename of output surface.
@@ -8,14 +8,12 @@ def smooth_surface(file_in, file_out, n_iter):
         
     created by Daniel Haenelt
     Date created: 13-07-2019
-    Last modified: 18-12-2019
+    Last modified: 25-08-2020
     """
-    from nipype.interfaces.freesurfer import SmoothTessellation
+    import os
          
-    # smooth surface
-    smooth = SmoothTessellation()
-    smooth.inputs.in_file = file_in
-    smooth.inputs.out_file = file_out
-    smooth.inputs.smoothing_iterations = n_iter
-    smooth.inputs.disable_estimates = True
-    smooth.run()
+    # smooth surface   
+    os.system("mris_smooth" + \
+              " -n "+str(n_iter) + \
+              " " + file_in + \
+              " " + file_out)
