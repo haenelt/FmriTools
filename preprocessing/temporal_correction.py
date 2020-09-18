cabe99465458db15c2fc29d0767a335da2285053
Daniel Haenelt <haenelt@madonna.cbs.mpg.de>
2020-09-18 10:10:55 +0200

    update

--- preprocessing/temporal_correction.py
+++ preprocessing/temporal_correction.py
@@ -13,10 +13,17 @@ Last modified: 11-03-2020
 from lib.preprocessing.slice_timing_correction import slice_timing_correction
 
 # input
-input = ["/data/pt_01880/Experiment2_Rivalry/p1/rivalry/GE_EPI1/Run_1/data.nii",
+input = ["/data/pt_01880/Experiment1_ODC/p4/retinotopy/pol_anticlock/data.nii",
+         "/data/pt_01880/Experiment1_ODC/p4/retinotopy/pol_clock/data.nii",
+         "/data/pt_01880/Experiment1_ODC/p4/retinotopy/ecc_expanding/data.nii",
+         "/data/pt_01880/Experiment1_ODC/p4/retinotopy/ecc_contracting/data.nii",
+         "/data/pt_01880/Experiment1_ODC/p4/retinotopy2/pol_anticlock/data.nii",
+         "/data/pt_01880/Experiment1_ODC/p4/retinotopy2/pol_clock/data.nii",
+         "/data/pt_01880/Experiment1_ODC/p4/retinotopy2/ecc_expanding/data.nii",
+         "/data/pt_01880/Experiment1_ODC/p4/retinotopy2/ecc_contracting/data.nii",
          ]
-TR_old = 1.5
-TR_new = 1
+TR_old = 2.0
+TR_new = 2.0
 order = "descending"
 
 """ do not edit below """
