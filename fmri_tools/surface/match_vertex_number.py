bfbf4b2a8cb05e449aed44f54bec5856ce58c7d3
haenelt <haenelt@cbs.mpg.de>
2020-10-24 10:35:25 +0200

    update

--- fmri_tools/surface/match_vertex_number.py
+++ fmri_tools/surface/match_vertex_number.py
@@ -8,8 +8,7 @@ import numpy as np
 from nibabel.freesurfer.io import read_geometry, write_geometry
 
     
-def match_vertex_number(input_white_surf, input_pial_surf, input_white_ind, 
-                        input_pial_ind, path_output):
+def match_vertex_number(vtx_white, vtx_pial, fac, ind_white, ind_pial):
     """ Match vertex number
     
     This function takes a white and a pial surface as input and matches the 
@@ -42,88 +41,109 @@ def match_vertex_number(input_white_surf, input_pial_surf, input_white_ind,
 
     """
     
-    # make output folder
-    if not os.path.exists(path_output):
-        os.makedirs(path_output)
+    # vertex indices in orig space which are in neither of both deformed 
+    # surfaces
+    ind_remove = list(set(ind_pial) - set(ind_white))
+    ind_remove.extend(set(ind_white) - set(ind_pial))
+    ind_remove = np.sort(ind_remove, rever=True) 
 
-    # load geometry
-    vtx_white, fac_white = read_geometry(input_white_surf)
-    vtx_pial, _ = read_geometry(input_pial_surf)
-
-    # load index file
-    white_ind = np.loadtxt(input_white_ind).astype(int)
-    pial_ind = np.loadtxt(input_pial_ind).astype(int)
-
-    # vertex indices in orig space which are in neither of both deformed surfaces
-    ind_remove = list(set(pial_ind) - set(white_ind))
-    ind_remove.extend(set(white_ind) - set(pial_ind))
-    ind_remove = np.sort(ind_remove) 
-
-    # sort vertices of white surface
-    i = 0
-    c_white = np.zeros_like(white_ind)
-    while i < np.size(white_ind):
-        if np.any(ind_remove == white_ind[i]):
-            c_white[i] = 1
+    ind_keep = list(set(fac) - set(ind_remove)).sort()
     
-        i += 1    
+    vtx_white = vtx_white[ind_keep,:]
+    vtx_pial = vtx_pial[ind_keep,:]
 
-    # sort vertices of pial surface
-    i = 0
-    c_pial = np.zeros_like(pial_ind)
-    while i < np.size(pial_ind):
-        if np.any(ind_remove == pial_ind[i]):
-            c_pial[i] = 1
-
-        i += 1  
-
-    # sort faces
-    fac_old = fac_white.copy()
-    fac_new = fac_white.copy()
-    fac_outlier = np.zeros_like(fac_white)
+    # get new faces
+    fac_keep = np.zeros(len(fac))
+    fac_keep += np.in1d(fac[:,0], ind_keep)
+    fac_keep += np.in1d(fac[:,1], ind_keep)
+    fac_keep += np.in1d(fac[:,2], ind_keep)
+    fac = fac[fac_keep == 3,:]
     
+    # reindex faces
     loop_status = 0
     loop_length = len(ind_remove)
     for i in range(loop_length):
-        check_remove = np.where(ind_remove[i] == white_ind)[0]
-        if len(check_remove):
-            row, col = np.where(fac_old == check_remove)
-            fac_outlier[row,col] = 1 # remember which faces to remove
-            fac_temp = fac_new.copy() # update face numbering
-            fac_temp[fac_old > check_remove] = -1
-            fac_temp[fac_temp != -1] = 0
-            fac_new += fac_temp
-    
-        i += 1
         
         # print status
         counter = np.floor(i / loop_length * 100)
         if counter != loop_status:
             print("sort faces: "+str(counter)+" %")
             loop_status = counter
+        
+        tmp = fac[fac >= ind_remove[i]] - 1
+        fac[fac >= ind_remove[i]] = tmp
+
+    return vtx_white, vtx_pial, fac
+
+    # sort vertices of white surface
+    #i = 0
+    #c_white = np.zeros_like(white_ind)
+    #c_pial = np.zeros_like(pial_ind)
+    #for i in range(len(ind_remove)):
+    #    c_white[white_ind==ind_remove[i]] = 1
+    #    c_pial[pial_ind==ind_remove[i]] = 1
+    
+    #while i < len(white_ind):
+    #    if np.any(ind_remove == white_ind[i]):
+    #        c_white[i] = 1
+    # 
+    #    i += 1    
+
+    # sort vertices of pial surface
+    #i = 0
+    #c_pial = np.zeros_like(pial_ind)
+    #while i < len(pial_ind):
+    #    if np.any(ind_remove == pial_ind[i]):
+    #        c_pial[i] = 1
+    #
+    #    i += 1  
+
+    # sort faces
+    #fac_old = fac_white.copy()
+    #fac_new = fac_white.copy()
+    #fac_outlier = np.zeros_like(fac_white)
+    
+    #loop_status = 0
+    #loop_length = len(ind_remove)
+    #for i in range(loop_length):
+    #    check_remove = np.where(ind_remove[i] == white_ind)[0]
+    #    if len(check_remove):
+    #        row, col = np.where(fac_old == check_remove)
+    #        fac_outlier[row,col] = 1 # remember which faces to remove
+    #        fac_temp = fac_new.copy() # update face numbering
+    #        fac_temp[fac_old > check_remove] = -1
+    #        fac_temp[fac_temp != -1] = 0
+    #        fac_new += fac_temp
+    #
+    #    i += 1
+    #    
+    #    # print status
+    #    counter = np.floor(i / loop_length * 100)
+    #    if counter != loop_status:
+    #        print("sort faces: "+str(counter)+" %")
+    #        loop_status = counter
 
     # remove outlier faces
-    fac_outlier = np.sum(fac_outlier,1)
-    fac_new = fac_new[fac_outlier == 0]
+    #fac_outlier = np.sum(fac_outlier,1)
+    #fac_new = fac_new[fac_outlier == 0]
 
     # remove outliers in vertices
-    vtx_white = vtx_white[c_white == 0]
-    vtx_pial = vtx_pial[c_pial == 0]
+    #vtx_white = vtx_white[c_white == 0]
+    #vtx_pial = vtx_pial[c_pial == 0]
 
     # remove outliers in ind
-    white_ind = white_ind[c_white == 0]
-    pial_ind = pial_ind[c_pial == 0]
+    #white_ind = white_ind[c_white == 0]
+    #pial_ind = pial_ind[c_pial == 0]
 
     # write output
-    name_output = os.path.basename(input_white_surf)
-    write_geometry(os.path.join(path_output, name_output+"_match"),vtx_white,fac_new)
+    #file_out = os.path.join(path_output, os.path.basename(input_white_surf))
+    #write_geometry(file_out+"_match", vtx_white, fac_new)
 
-    name_output = os.path.basename(input_pial_surf)
-    write_geometry(os.path.join(path_output, name_output+"_match"),vtx_pial,fac_new)
+    #file_out = os.path.join(path_output, os.path.basename(input_pial_surf))
+    #write_geometry(file_out+"_match", vtx_pial, fac_new)
     
-    name_output = os.path.basename(input_white_ind)
-    np.savetxt(os.path.join(path_output, name_output+"_match.txt"), white_ind, fmt='%d')
+    #file_out = os.path.join(path_output, os.path.basename(input_white_ind))
+    #np.savetxt(file_out+"_match.txt", white_ind, fmt='%d')
 
-    name_output = os.path.basename(input_pial_ind)
-    np.savetxt(os.path.join(path_output, name_output+"_match.txt"), pial_ind, fmt='%d')
-    
+    #file_out = os.path.join(path_output, os.path.basename(input_pial_ind))
+    #np.savetxt(file_out+"_match.txt", pial_ind, fmt='%d')
