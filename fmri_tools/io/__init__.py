21cddbf95d860d948887b4de83dcf5528da8a745
haenelt <haenelt@cbs.mpg.de>
2021-05-21 10:32:51 +0200

    update init

--- fmri_tools/io/__init__.py
+++ fmri_tools/io/__init__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 # local inputs
-#from .copy_header import copy_header
-#from .get_filename import get_filename
-#from .mgh2nii import mgh2nii
-#from .read_patch import read_patch
-#from .read_vox2vox import read_vox2vox
-#from .write_label import write_label
-#from .read_mgh import read_mgh
-#from .write_mgh import write_mgh
-#from .read_hdf5 import read_hdf5
-#from .write_hdf5 import write_hdf5
-#from .extract_mgh_from_hdf5 import extract_mgh_from_hdf5
-#from .write_vector_field import write_vector_field
+from .copy_header import copy_header
+from .get_filename import get_filename
+from .mgh2nii import mgh2nii
+from .read_patch import read_patch
+from .read_vox2vox import read_vox2vox
+from .write_label import write_label
+from .read_mgh import read_mgh
+from .write_mgh import write_mgh
+from .read_hdf5 import read_hdf5
+from .write_hdf5 import write_hdf5
+from .extract_mgh_from_hdf5 import extract_mgh_from_hdf5
+from .write_vector_field import write_vector_field
 
 
 """
--- fmri_tools/layer/__init__.py
+++ fmri_tools/layer/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 # local inputs
-#from .calc_equidist import calc_equidist
-#from .calc_equivol import calc_equivol
-#from .calc_equivol2 import calc_equivol2
-#from .calc_equivol3 import calc_equivol3
-#from .calc_equidist_surf import calc_equidist_surf
-#from .calc_equivol_surf import calc_equivol_surf
-#from .get_meshlines import get_meshlines
+from .calc_equidist import calc_equidist
+from .calc_equivol import calc_equivol
+from .calc_equivol2 import calc_equivol2
+from .calc_equivol3 import calc_equivol3
+from .calc_equidist_surf import calc_equidist_surf
+from .calc_equivol_surf import calc_equivol_surf
+from .get_meshlines import get_meshlines
 
 
 """
