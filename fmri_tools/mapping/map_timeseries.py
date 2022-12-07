ba0a730a9f15948247ce82388fce186e95882a52
Daniel Haenelt <haenelt@maki.cbs.mpg.de>
2022-12-07 22:43:32 +0100

    update

--- fmri_tools/mapping/map_timeseries.py
+++ fmri_tools/mapping/map_timeseries.py
@@ -121,15 +121,20 @@ def _careful_divide(v, v0, v1):
 
 
 if __name__ == "__main__":
+    # mache ein bisschen schoener
+    # choose interpolation method
+    # cmap optional
+    # label optional
+
     from fmri_tools.io.affine import read_vox2ras_tkr
-    from nibabel.affines import apply_affine
+    from fmri_tools.utils.apply_affine_chunked import apply_affine_chunked
+    from nibabel.freesurfer.io import read_label
+    from fmri_tools.io.surf import write_mgh
 
-    file_surf = "/data/pt_01880/zdata/lh.layer_0"
-    file_cmap = "/data/pt_01880/zdata/source2target.nii.gz"
-    file_timeseries = "/data/pt_01880/zdata/uadata.nii"
-
-    # convert to voxel space
-    # what if it exceeds the boundaries
+    file_surf = "/data/pt_01880/zdata2/lh.white_def1"
+    file_cmap = "/data/pt_01880/zdata2/epi2ana.nii.gz"
+    file_timeseries = "/data/pt_01880/zdata2/udata.nii"
+    file_label = "/data/pt_01880/zdata2/lh.V1_exvivo.label"
 
     mesh = Mesh.from_file(file_surf)
     cmap = nb.load(file_cmap)
@@ -137,32 +142,50 @@ if __name__ == "__main__":
     nx, ny, nz, _ = np.shape(arr_cmap)
     data = nb.load(file_timeseries)
     arr = data.get_fdata()
-    _, _, _, nt = np.shape(arr)
+    label = read_label(file_label)
 
-    vtx = mesh.verts
-    vox2ras, ras2vox = read_vox2ras_tkr(file_cmap)
-    vtx_vox = apply_affine(ras2vox, vtx)
+    _, ras2vox = read_vox2ras_tkr(file_cmap)
+    vox2ras, _ = read_vox2ras_tkr(file_timeseries)
+    vtx_vox = apply_affine_chunked(ras2vox, mesh.verts)
 
-    mask = np.zeros(len(vtx))
-    mask[vtx_vox[:, 0] >= 0] = 1
-    mask[vtx_vox[:, 1] >= 0] = 1
-    mask[vtx_vox[:, 2] >= 0] = 1
-    mask[vtx_vox[:, 0] <= nx] = 1
-    mask[vtx_vox[:, 1] <= ny] = 1
-    mask[vtx_vox[:, 2] <= nz] = 1
+    mask = np.arange(len(mesh.verts))
+    mask[vtx_vox[:, 0] < 0] = -1
+    mask[vtx_vox[:, 1] < 0] = -1
+    mask[vtx_vox[:, 2] < 0] = -1
+    mask[vtx_vox[:, 0] > nx] = -1
+    mask[vtx_vox[:, 1] > ny] = -1
+    mask[vtx_vox[:, 2] > nz] = -1
+    mask = mask[mask != -1]
 
+    vtx_vox = vtx_vox[mask, :]
     x = linear_interpolation3d(vtx_vox[:,0], vtx_vox[:,1], vtx_vox[:,2], arr_cmap[:,:,:,0])
     y = linear_interpolation3d(vtx_vox[:,0], vtx_vox[:,1], vtx_vox[:,2], arr_cmap[:,:,:,1])
     z = linear_interpolation3d(vtx_vox[:,0], vtx_vox[:,1], vtx_vox[:,2], arr_cmap[:,:,:,2])
 
-    vtx_res = np.zeros_like(vtx)
-    vtx_res[mask == 1, 0] = x
-    vtx_res[mask == 1, 1] = y
-    vtx_res[mask == 1, 2] = z
+    vtx_vox = np.array([x, y, z]).T
+    nx, ny, nz, nt = np.shape(arr)
 
-    vtx_res = apply_affine(vox2ras, vtx_res)
+    mask[vtx_vox[:, 0] < 0] = -1
+    mask[vtx_vox[:, 1] < 0] = -1
+    mask[vtx_vox[:, 2] < 0] = -1
+    mask[vtx_vox[:, 0] > nx] = -1
+    mask[vtx_vox[:, 1] > ny] = -1
+    mask[vtx_vox[:, 2] > nz] = -1
+    mask = mask[mask != -1]
 
-    from nibabel.freesurfer import write_geometry
-    write_geometry("/data/pt_01880/zzz", vtx_res, mesh.faces)
+    #vtx_res = apply_affine_chunked(vox2ras, vtx_res)
+    #from nibabel.freesurfer import write_geometry
+    #write_geometry("/data/pt_01880/zzz", vtx_res, mesh.faces)
 
-    res = nn_interpolation3d(x, y, z, arr[:, :, :, 0])
+    vtx_vox = vtx_vox[mask, :]
+    res = nn_interpolation3d(vtx_vox[:,0], vtx_vox[:,1], vtx_vox[:,2], arr[:, :, :, 0])
+    res_overlay = np.zeros(len(mesh.verts))
+    res_overlay[mask] = res
+
+
+
+    mask2 = np.ones(len(mesh.verts), dtype=int)
+    mask2[label] = 0
+    res_overlay[mask2 == 1] = 0
+
+    write_mgh("/data/pt_01880/zzz2.mgh", res_overlay)
